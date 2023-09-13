d3a767af3e1be4b5763419c43e5c8e1e41237c3a
ASUS <ASUS@DESKTOP-HTLAL2F>
2023-09-13 15:46:27 +0530

    branch committed

--- src/test/java/practise/Demo1.java
+++ src/test/java/practise/Demo1.java
@@ -2,7 +2,9 @@ package practise;
 
 public class Demo1 
 {
-public static void main(String[] args) {
+public static void main(String[] args)
+{
 	System.out.println("updated");
+	System.out.println("branch committed");
 }
 }
